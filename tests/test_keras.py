4b35f88a83653a1931da49ae560168608ad4c467
Sam <sam.vanhoutte@marchitec.be>
2020-04-24 16:25:28 +0200

    updated tests to skip gpu test when not available

--- tests/test_keras.py
+++ tests/test_keras.py
@@ -8,7 +8,17 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 mylogger = logging.getLogger()
 
+def check_gpu():
+    from tensorflow.python.client import device_lib
+
+    local_device_protos = device_lib.list_local_devices()
+    return len([x.name for x in local_device_protos if x.device_type == 'GPU']) > 0
 
 def test_gpu():
+    if not check_gpu():
+        import pytest
+        pytest.skip('Test requires GPU')
+
     # print dataframe. 
     assert nnk.enable_gpu() == True
+
