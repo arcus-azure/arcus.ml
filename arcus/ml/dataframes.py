0545c56dbeb39575d197c50d9dd30b8271c142c9
Sam <sam.vanhoutte@marchitec.be>
2020-04-30 09:31:22 +0200

    Added onehotencoding (Issue #28)

--- arcus/ml/dataframes.py
+++ arcus/ml/dataframes.py
@@ -16,3 +16,27 @@ def shuffle(df: pd.DataFrame) -> pd.DataFrame:
 
     return df.sample(frac=1).reset_index(drop=True)
 
+def one_hot_encode(df: pd.DataFrame, column_name: str, drop_column:bool = True, prefix: str = None):
+    '''Take a categorical column and pivots the DataFrame to add columns (0 or 1 value) for every category
+
+    Args:
+        df (pd.DataFrame): The DataFrame that contains the column to be encoded
+        column_name (str): The name of the column that contains the categorical values
+        drop_column (bool): Will remove the original column from the dataframe
+        prefix (str): The prefix of the new columns.  By default the original column name will be taken
+
+    Returns: 
+        pd.DataFrame: The DataFrame with the one hot encoded features
+    '''
+    # Apply logic for previx columns
+    if prefix == None:
+        prefix = column_name
+
+    # Apply one hot encoding
+    df = pd.concat(
+        [df, pd.get_dummies(df[column_name], prefix=prefix)], axis=1)
+
+    if(drop_column):
+        df.drop([column_name], axis=1, inplace=True)
+
+    return df
--- tests/test_dataframes.py
+++ tests/test_dataframes.py
@@ -8,6 +8,10 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 mylogger = logging.getLogger()
 
+# initialize list of lists 
+categorical_data = [['BE', 10], ['FR', 15], ['BE', 14], ['UK', 14], ['SE', 14], ['SE', 14]] 
+
+
 def setup_module(module):
     ''' Setup for the entire module '''
     # Do the actual setup stuff here
@@ -28,3 +32,36 @@ def test_shuffle():
     shuffled_df = adf.shuffle(test_df)
     assert len(shuffled_df)==len(test_df)
     assert shuffled_df[shuffled_df.Name == 'nick'].iloc[0].Age == 15
+
+def test_one_hot_encoding_default():
+    # Create the pandas DataFrame 
+    test_df = pd.DataFrame(categorical_data, columns = ['Country', 'Infected']) 
+
+    # One hot encode the Country column
+    encoded_df = adf.one_hot_encode(test_df, 'Country')
+    assert len(encoded_df.columns) == 5
+    assert 'Country_BE' in encoded_df.columns
+    assert len(encoded_df.loc[encoded_df['Country_BE'] == 1]) == 2
+    assert len(encoded_df.loc[encoded_df['Country_FR'] == 0]) == 5
+
+
+def test_one_hot_encoding_keepcolumn():
+    # Create the pandas DataFrame 
+    test_df = pd.DataFrame(categorical_data, columns = ['Country', 'Infected']) 
+
+    # One hot encode the Country column
+    encoded_df = adf.one_hot_encode(test_df, 'Country', drop_column=False)
+    assert len(encoded_df.columns) == 6
+    assert 'Country_SE' in encoded_df.columns
+    assert len(encoded_df.loc[encoded_df['Country_UK'] == 1]) == 1
+
+
+def test_one_hot_encoding_prefix():
+    # Create the pandas DataFrame 
+    test_df = pd.DataFrame(categorical_data, columns = ['Country', 'Infected']) 
+    encoded_df = adf.one_hot_encode(test_df, 'Country', prefix='cnt')
+
+    # One hot encode the Country column
+    assert len(encoded_df.columns) == 5
+    assert 'cnt_BE' in encoded_df.columns
+    assert len(encoded_df.loc[encoded_df['cnt_UK'] == 1]) == 1
