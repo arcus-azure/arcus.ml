380ec10d1b7911d3df97c1db724939774de3d1fe
Sam <sam.vanhoutte@marchitec.be>
2020-05-09 18:52:43 +0200

    add load from dataframe image

--- arcus/ml/images/io.py
+++ arcus/ml/images/io.py
@@ -10,6 +10,7 @@ import requests
 from skimage import transform
 from arcus.ml.images import conversion
 from io import BytesIO
+import pandas as pd
 
 _logger = logging.getLogger()
 
@@ -109,3 +110,35 @@ def load_image_from_url(image_url: str, http_headers: dict = None, image_size:in
                 return conversion.prepare(image, image_size, convert_to_grey, keep_3d_shape)
             else:
                 raise Exception('HTTP error: ' + str(_http_response.status_code))
+
+def load_images_from_dataframe(df: pd.DataFrame, image_column_name:str, target_column_name: str, 
+                                image_size:int = -1, max_images: int = -1, 
+                                convert_to_grey: bool = False, keep_3d_shape = False) -> (np.array, np.array):
+    '''
+    Loads the images from a specific folder
+    Args:
+        path (str): The path or folder name to load images from.  This can be a relative or fully qualified path
+        image_size (int): If the image_size is larger than 0, the images will be resized to a square of this size
+        max_images (int): The maximum amount of images to load from the folder.  If 0 or smaller, all images will be returned
+        valid_extensions (np.array): The file extensions that should be filtered.  Defaults to jpg, jpeg, gif and png
+        convert_to_grey (bool): This would reduce the size (and shape) of the image in making it a greyscale
+        keep_3d_shape (bool): Only used when convert_to_grey is true.  Will keep the images in shape (H,W,1) in that case
+    Returns: 
+        np.array: A numpy array that contains all selected images represented as np.array
+    '''
+    images = []
+    targets = []
+    for idx, row in df.iterrows() :
+        file = row[image_column_name]
+        if file and os.path.exists(file):
+
+            im = load_image_from_disk(file, image_size, convert_to_grey, keep_3d_shape)
+            images.append(im)
+            targets.append(row[target_column_name])
+        else:
+            _logger.warning('File ' + file + 'not found')
+
+        if(len(images) >= max_images and max_images > 0):
+            break
+    
+    return np.array(images), np.array(targets)
