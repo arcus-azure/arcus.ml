74a99af09e3789d07e2d97599841363f78d695da
Sam <sam.vanhoutte@marchitec.be>
2020-06-11 17:04:52 +0200

    return plot instead of visualize, in silent mode

--- tests/test_images_explorer.py
+++ tests/test_images_explorer.py
@@ -39,3 +39,37 @@ def test_show_3d_shape_image():
 #X = conversion.to_blackwhite(X, keep_3d_shape=True, threshold=200)
     exp.show_image(lungs[1])
 
+def test_visualize_random_images_silent():
+    lungs = imgio.load_images(image_path_1)
+    masks = imgio.load_images(image_path_2)
+
+    pp = exp.visualize({'lungs': lungs, 'masks': masks}, image_count = 6, randomize=True, silent_mode=True)
+    assert pp is not None
+
+def test_visualize_first_images_silent():
+    lungs = imgio.load_images(image_path_1)
+    masks = imgio.load_images(image_path_2)
+
+    pp = exp.visualize({'lungs': lungs, 'masks': masks}, image_count = 6, randomize=False, silent_mode=True)
+    assert pp is not None
+
+def test_visualize_3d_shape_images_silent():
+    lungs = imgio.load_images(image_path_1, convert_to_grey=True, keep_3d_shape=True)
+    masks = imgio.load_images(image_path_2, convert_to_grey=True, keep_3d_shape=True)
+
+    pp = exp.visualize({'lungs': lungs, 'masks': masks}, image_count = 6, randomize=True, silent_mode=True)
+    assert pp is not None
+
+
+def test_visualize_oneset_images_silent():
+    lungs = imgio.load_images(image_path_1)
+
+    pp = exp.visualize({'lungs': lungs}, image_count = 6, randomize=True, silent_mode=True)
+    assert pp is not None
+
+def test_show_3d_shape_image_silent():
+    lungs = imgio.load_images(image_path_1, convert_to_grey=True, keep_3d_shape=True)
+
+    pp = exp.show_image(lungs[1], silent_mode=True)
+    assert pp is not None
+
