649a90932c50d6e450cebf870cb90b41a35ffba8
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-02-03 09:32:56 +0100

    test: add time windows properties for improved implementation

--- arcus/ml/timeseries/timeops.py
+++ arcus/ml/timeseries/timeops.py
@@ -53,7 +53,13 @@ def time_slice(df: pd.DataFrame, time_column:str, start: datetime.datetime = Non
     df = df.sort_index()
     return df.loc[start:end]
 
-def get_windows(sorted_df: pd.DataFrame, window_size: int, window_stride: int = 1, group_column: str = None, zero_padding: bool = False, remove_group_column: bool = False, target_column: str = None) -> np.array:
+def get_windows(sorted_df: pd.DataFrame, 
+                window_size: int, 
+                window_stride: int = 1, 
+                group_column: str = None, 
+                zero_padding: bool = False, 
+                remove_group_column: bool = False, 
+                target_column: str = None) -> np.array:
     '''
     This method take a DataFrame and returns a set of time windows of a specific length and a given column, eventually grouped by another column
 
@@ -71,6 +77,13 @@ def get_windows(sorted_df: pd.DataFrame, window_size: int, window_stride: int = 
         np.array: An array, with all the linked target values
     '''
 
+    if window_size <= 0: 
+        raise ValueError('Requires a positive (> 0 ) window_size for getting time windows of a data frame')
+    if group_column is not None and group_column not in sorted_df.columns:
+        raise ValueError('Requires a group_column when getting grouped time windows that is presnet in the data frame')
+    if target_column is not None and target_column not in sorted_df.columns:
+        raise ValueError('Requires a target_column when getting targeted windows that is present in the data frame ' + target_column)
+
     if group_column is None:
         return __get_windows_from_group(sorted_df, window_size, window_stride, zero_padding, target_column)
 
@@ -78,15 +91,16 @@ def get_windows(sorted_df: pd.DataFrame, window_size: int, window_stride: int = 
         windows = None
         targets = None
         
-        # create unique list of groups
-        _groups = sorted_df[group_column].unique()
+        _unique_groups = sorted_df[group_column].unique()
 
-        for key in _groups:
+        for key in _unique_groups:
             _group_df = sorted_df[:][sorted_df[group_column] == key]
-            if(remove_group_column):
+            if remove_group_column:
                 _group_df.drop(group_column, axis=1, inplace=True)
+            
             _current_windows, _current_targets = __get_windows_from_group(
                 _group_df, window_size, window_stride, zero_padding, target_column)
+            
             if windows is None:
                 windows = _current_windows
             else:
@@ -105,36 +119,39 @@ def __get_windows_from_group(sorted_df: pd.DataFrame, window_size: int,
                              target_column:str = None) -> np.array:
     # zero based row to take the leading window from - this row will be last row in the window
     _start_row_idx = 0 if zero_padding else window_size - 1
-    # will contain all windows
+    _row_count = len(sorted_df)
+
     windows = list()
     targets = list()
     
-    # range will be from start_row to row_count
-    for __current_row_idx in range(_start_row_idx, len(sorted_df)):
-        __slice_begin_idx = (__current_row_idx - window_size +
-                             1) if __current_row_idx >= window_size else 0
+    for _current_row_idx in range(_start_row_idx, _row_count):
+        _slice_begin_idx = (_current_row_idx - window_size + 1) if _current_row_idx >= window_size else 0
+        
         window_df = sorted_df.copy()
-        time_slice = window_df.iloc[__slice_begin_idx:__current_row_idx + 1, :]
+
+        time_slice = window_df.iloc[_slice_begin_idx:_current_row_idx + 1, :]
         time_array = np.array(time_slice.values)
-        if(zero_padding):
-            _rows_to_pad = window_size - __current_row_idx - 1
-            if(_rows_to_pad > 0):
-                padding_matrix = np.zeros(
-                    (_rows_to_pad, len(sorted_df.columns)))
+        
+        if zero_padding:
+            _rows_to_pad = window_size - _current_row_idx - 1
+            if _rows_to_pad > 0:
+                padding_matrix = np.zeros((_rows_to_pad, len(sorted_df.columns)))
                 time_array = np.concatenate((padding_matrix, time_array))
-        if(target_column is not None):
+        
+        if target_column is not None:
             _target_colidx = sorted_df.columns.get_loc(target_column)
             time_array, target_array = __pop_from_array(time_array, _target_colidx)
             targets.append(target_array)
 
         windows.append(time_array)
+    
     print(len(targets))
     return np.array(windows), np.array(targets) if target_column is not None else None
 
-def __pop_from_array(my_array,pc):
+def __pop_from_array(my_array, pc):
     i = pc
-    pop = my_array[:,i]
-    new_array = np.hstack((my_array[:,:i],my_array[:,i+1:]))
+    pop = my_array[:, i]
+    new_array = np.hstack((my_array[:, :i], my_array[:, i+1:]))
     return new_array, pop
 
 
--- tests/test_timeseries.py
+++ tests/test_timeseries.py
@@ -8,6 +8,8 @@ import logging
 from unittest.mock import patch 
 from datetime import datetime
 import numpy as np 
+from hypothesis import given, strategies as st, assume
+from hypothesis.extra.pandas import data_frames, columns
 
 logging.basicConfig(level=logging.DEBUG)
 mylogger = logging.getLogger()
@@ -40,7 +42,6 @@ def test_time_slice_both_str():
     assert slice_df.shape == (30, 3)
     slice_df = tops.time_slice(df, 'Date', start='01/01/2016', end='01/01/2017')
     assert slice_df.shape == (12, 3)
-
 
 def test_time_slice_start_date():
     df = time_series_df.copy()
@@ -82,7 +83,34 @@ def test_add_empty_df_timerange():
     full_range = tops.combine_time_ranges(df00, df01)
     assert len(full_range) == 6
 
-def test_time_windows_default():
+class WinGroup():
+    def __init__(self, column_name, strategy):
+        self.column_name = column_name
+        self.strategy = strategy
+    def assign(self, draw, df):
+        df[self.column_name] = draw(self.strategy)
+
+@st.composite
+def tables(draw, win_group = None):
+    colls_length = draw(st.integers(min_value=1, max_value=10))
+    colls_names = ["engine_id", "cycle", "setting1", "setting2", "sensor4", "seonsor4", "sensor10"]
+    colls = draw(st.lists(st.sampled_from(colls_names), min_size=colls_length, max_size=colls_length))
+    df = draw(data_frames(columns=columns(colls_names, dtype=float, elements=st.floats(allow_nan=False))))
+    if win_group is not None:
+        win_group.assign(draw, df)
+    assume(len(df) > 1)
+    return df
+
+def get_random_window_size_within(df, data):
+    return data.draw(st.integers(min_value=1, max_value=len(df)))
+
+@given(tables(), st.data())
+def test_time_windows_default_property(df, data):
+    win_size = get_random_window_size_within(df, data)
+    windows, _ = tops.get_windows(df, win_size)
+    assert_all_windows_equals(df, windows, win_size)
+
+def test_time_windows_default_example():
     df = pd.read_csv('tests/resources/datasets/engine-sensors.csv')
     assert len(df)==20  
     assert df.shape == (20, 27)  
@@ -90,8 +118,14 @@ def test_time_windows_default():
     col_size = len(df.columns)
     windows, _ = tops.get_windows(df, win_size)
     assert windows.shape == (15, win_size, col_size)
+    assert_all_windows_equals(df, windows, win_size)
 
-def test_time_windows_targets():
+@given(tables(), st.integers(max_value=0))
+def test_time_windows_negative_windows_size_fails(df, win_size):
+    with pytest.raises(ValueError, match=r'.* window_size .*'):
+        tops.get_windows(df, win_size)
+
+def test_time_windows_targets_example():
     df = pd.read_csv('tests/resources/datasets/engine-sensors.csv')
     assert len(df)==20  
     assert df.shape == (20, 27)  
@@ -100,8 +134,24 @@ def test_time_windows_targets():
     windows, targets = tops.get_windows(df, win_size, target_column='ttf')
     assert windows.shape == (15, win_size, col_size - 1)
     assert targets.shape == (15, win_size)
+    assert_all_windows_equals(df.drop('ttf', axis=1), windows, win_size)
+    assert_all_windows_equals(df['ttf'], targets, win_size)
 
-def test_time_windows_group_targets():
+@given(tables(), st.data())
+def test_time_windows_targets_property(df : pd.DataFrame, data):
+    win_size = get_random_window_size_within(df, data)
+    target_column_example = data.draw(st.sampled_from(list(df.columns)))
+    windows, targets = tops.get_windows(df, win_size, target_column=target_column_example)
+    assert_all_windows_equals(df.drop(target_column_example, axis=1), windows, win_size)
+    assert_all_windows_equals(df[target_column_example], targets, win_size)
+
+@given(tables(), st.data())
+def test_time_windows_nonpresent_targets_property(df, data):
+    win_size = get_random_window_size_within(df, data)
+    with pytest.raises(ValueError, match=r'.* target_column .*'):
+        tops.get_windows(df, win_size, target_column='non_present_column')
+
+def test_time_windows_group_targets_example():
     df = pd.read_csv('tests/resources/datasets/engine-sensors.csv')
     assert len(df)==20  
     assert df.shape == (20, 27)  
@@ -110,8 +160,17 @@ def test_time_windows_group_targets():
     windows, targets = tops.get_windows(df, win_size, group_column='engine_id', remove_group_column=True, target_column='ttf')
     assert windows.shape == (10, win_size, col_size - 2)
     assert targets.shape == (10, win_size)
+    assert_all_windows_equals_with_group_targets(df, windows, targets, win_size, group_column='engine_id', target_column='ttf')
 
-def test_time_windows_padding():
+group_column_example = 'device_id'
+@given(tables(win_group=WinGroup(group_column_example, st.sampled_from([0, 1]))), st.data())
+def test_time_windows_group_targets_property(df, data):
+    win_size = get_random_window_size_within(df, data)
+    target_column_example = data.draw(st.sampled_from(list(df.columns[df.columns != group_column_example])))
+    windows, targets = tops.get_windows(df, win_size, group_column=group_column_example, remove_group_column=True, target_column=target_column_example)
+    assert_all_windows_equals_with_group_targets(df, windows, targets, win_size, group_column_example, target_column_example)
+
+def test_time_windows_padding_example():
     df = pd.read_csv('tests/resources/datasets/engine-sensors.csv')
     assert len(df)==20  
     assert df.shape == (20, 27)  
@@ -119,17 +178,104 @@ def test_time_windows_padding():
     col_size = len(df.columns)
     windows, _ = tops.get_windows(df, win_size, zero_padding=True)
     assert windows.shape == (20, win_size, col_size)
+    assert_all_windows_equals_with_padding(df, windows, win_size)
 
-def test_time_windows_group():
+@given(tables(), st.data())
+def test_time_windows_padding_property(df, data):
+    win_size = get_random_window_size_within(df, data)
+    windows, _ = tops.get_windows(df, win_size, zero_padding=True)
+    assert_all_windows_equals_with_padding(df, windows, win_size)
+
+def test_time_windows_group_example():
     df = pd.read_csv('tests/resources/datasets/engine-sensors.csv')
     win_size = 6
     col_size = len(df.columns)
     windows, _ = tops.get_windows(df, win_size, group_column='engine_id')
     assert windows.shape == (10, win_size, col_size)
+    assert_all_windows_equals_with_grouping(df, windows, win_size, 'engine_id')
 
-def test_time_windows_group_padding():
+@given(tables(win_group=WinGroup(group_column_example, st.sampled_from([0, 1]))), st.data())
+def test_time_windows_group_property(df, data):
+    win_size = get_random_window_size_within(df, data)
+    windows, _ = tops.get_windows(df, win_size, group_column=group_column_example)
+    assert_all_windows_equals_with_grouping(df, windows, win_size, group_column_example)
+
+@given(tables(win_group=WinGroup(group_column_example, st.just(0))), st.data())
+def test_time_windows_single_group_property(df, data):
+    win_size = get_random_window_size_within(df, data)
+    windows_group, _ = tops.get_windows(df, win_size, group_column=group_column_example)
+    windows, _ = tops.get_windows(df, win_size)
+    assert_sequence_equals(windows_group, windows)
+
+@given(tables(), st.data())
+def test_time_windows_nonpresent_group_property(df, data):
+    win_size = get_random_window_size_within(df, data)
+    with pytest.raises(ValueError, match=r'.* group_column .*'):
+        tops.get_windows(df, win_size, group_column='non_exist_column')
+
+def test_time_windows_group_padding_example():
     df = pd.read_csv('tests/resources/datasets/engine-sensors.csv')
     win_size = 6
     col_size = len(df.columns)
     windows, _ = tops.get_windows(df, win_size, group_column='engine_id', zero_padding=True)
     assert windows.shape == (20, win_size, col_size)
+    assert_all_windows_equals_with_padding_grouping(df, windows, win_size, 'engine_id')
+
+@given(tables(win_group=WinGroup(group_column_example, st.sampled_from([0, 1]))), st.data())
+def test_time_windows_group_padding_property(df, data):
+    win_size = get_random_window_size_within(df, data)
+    windows, _ = tops.get_windows(df, win_size, group_column=group_column_example, zero_padding=True)
+    assert_all_windows_equals_with_padding_grouping(df, windows, win_size, group_column_example)
+
+def assert_all_windows_equals(df, windows, win_size):
+    expected_windows = list()
+    add_windows(expected_windows, df, win_size)
+    assert_sequence_equals(expected_windows, windows)
+
+def assert_all_windows_equals_with_padding(df, windows, win_size):
+    expected_windows = list()
+    add_padding_windows(expected_windows, df, win_size)
+    add_windows(expected_windows, df, win_size)
+    assert_sequence_equals(expected_windows, windows)
+
+def add_padding_windows(expected_windows, df, win_size):
+    for win_index in range(1, win_size):
+        expected_values = df[0:win_index].values
+        padding = np.zeros((win_size - win_index, len(df.columns)))
+        expected_window = np.concatenate((padding, expected_values))
+        expected_windows.append(expected_window)
+
+def assert_all_windows_equals_with_grouping(df, windows, win_size, group_column):
+    expected_windows = list()
+    for name, group in df.groupby(group_column):
+        add_windows(expected_windows, group, win_size)
+    assert_sequence_equals(expected_windows, windows)
+
+def assert_all_windows_equals_with_padding_grouping(df, windows, win_size, group_column):
+    expected_windows = list()
+    for name, group in df.groupby(group_column):
+        add_padding_windows(expected_windows, group, win_size)
+        add_windows(expected_windows, group, win_size)
+    assert_sequence_equals(expected_windows, windows)
+
+def assert_all_windows_equals_with_group_targets(df, windows, targets, win_size, group_column, target_column):
+    expected_windows = list()
+    expected_targets = list()
+    for name, group in df.groupby(group_column):
+        add_windows(expected_windows, group.drop([group_column, target_column], axis=1), win_size)
+        add_windows(expected_targets, group[target_column], win_size)
+    assert_sequence_equals(expected_windows, windows)
+    assert_sequence_equals(expected_targets, targets)
+
+def add_windows(expected_windows, df, win_size):
+    for win_index in range(win_size, len(df) + 1):
+        index = win_index - win_size
+        expected_window = df[index:win_index].values
+        expected_windows.append(expected_window)
+
+def assert_sequence_equals(expected_seq, actual_seq):
+    assert len(expected_seq) == len(actual_seq)
+    for i in range(0, len(expected_seq)):
+        expected = expected_seq[i]
+        actual = actual_seq[i]
+        assert np.equal(expected, actual).all()
